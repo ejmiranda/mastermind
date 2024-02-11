e7d021eca427b0e68ac6be552ecdd42b45dd24ae
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-02-11 08:18:56 -0500

    Add properties and behavior to Player class

--- player.rb
+++ player.rb
@@ -2,4 +2,11 @@
 
 # A single player of a game
 class Player
+  attr_accessor :name, :id, :points
+
+  def initialize(name = '', id = '', points = 0)
+    @name = name
+    @id = id
+    @points = points
+  end
 end
