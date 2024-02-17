9878af79f6d7a5cf74fb5020f1fcf4d05f15b09a
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-02-17 08:01:25 -0500

    Create a method to validate a combination of valid values

--- input.rb
+++ input.rb
@@ -11,4 +11,14 @@ module Input
       puts invalid_msg
     end
   end
+
+  def get_valid_value_comb(prompt:, valid_values:, invalid_msg:, up_case: false)
+    puts prompt
+    loop do
+      value = up_case ? gets.chomp.upcase : gets.chomp
+      return value if value.split('').all? { |c| valid_values.include?(c) }
+
+      puts invalid_msg
+    end
+  end
 end
