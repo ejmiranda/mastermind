98eac345049a0614da6d10de9b6f9fafa8c64e38
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-04-21 06:34:10 -0500

    Add module with structure to add methods for comp to guess intelligently

--- comp_player.rb
+++ comp_player.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'player'
+require_relative 'smarts'
 
 # A single computer player of a game
 class CompPlayer < Player
+  include Smarts
   def initialize
     super(name: 'Comp')
   end
---
+++ smarts.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# A set of methods for Comp Player to win at Mastermind.
+module Smarts
+end
