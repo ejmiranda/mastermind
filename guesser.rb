9b05a51a0b9a6f6e9b846f192e22acae967c554d
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-04-27 07:50:56 -0500

    Turn set into a hash with numeric values as keys (1111, 1112, ... 1121, etc.)

--- guesser.rb
+++ guesser.rb
@@ -5,14 +5,38 @@ class Guesser
   attr_accessor :set
 
   def initialize
-    @set = []
+    @set = {}
   end
 
   def prepare_set(values:, digits: 4)
-    values.repeated_permutation(digits) { |permutation| set << permutation }
+    key = ('1' * digits).to_i
+    slice_digit = values.length
+    slice_step = 11 - slice_digit # Don't know what happens if values.length > 11
+    values.repeated_permutation(digits) do |permutation|
+      set[key] = permutation
+      key += key.to_s.slice(-1) == slice_digit.to_s ? slice_step : 1
+      break if key > 1125
+    end
   end
 
   def guess(id:)
-    puts "Guess ID: #{id}"
+    case id
+    when 1
+      print_set(10)
+      # puts find_in_set(%i[B B Y O])
+    end
+  end
+
+  def print_set(max_row)
+    row = 1
+    set.each do |key, value|
+      puts "#{row} - #{key} #{value}"
+      row += 1
+      break if row > max_row
+    end
+  end
+
+  def find_in_set(find_item)
+    set.find_index { |item| item == find_item }
   end
 end
